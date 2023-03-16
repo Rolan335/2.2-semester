b99341dd17561a716414129eb57ca020e525b5bb
Rolan Zaripov <buldozerr21@gmail.com>
2023-03-16 15:05:42 +0300

    i did everything wrong, here is the fix

--- JavaScript_DOM/2023.03.16(kt3)/script.js
+++ JavaScript_DOM/2023.03.16(kt3)/script.js
@@ -3,6 +3,36 @@ const card = document.querySelector(".card")
 const system = document.querySelector(".card__system")
 form.addEventListener("submit", e => {
     e.preventDefault()
+    card.style.backgroundImage = ""
+    system.style.backgroundImage = ""
+    document.querySelector(".card__number").textContent = ""
+    document.querySelector(".card__expire").textContent = ""
+    document.querySelector(".card__name").textContent = ""
+    addTable(form)
+    form.reset()
+    // e.preventDefault()
+    // switch (form[0].value.toUpperCase()) {
+    //     case "ТИНЬКОФФ":
+    //         card.style.backgroundImage = "url('img/1.png')"
+    //         break
+    //     case "СБЕРБАНК":
+    //         card.style.backgroundImage = "url('img/2.png')"
+    //         break
+    // }
+    // switch (form[1].value.toUpperCase()) {
+    //     case "VISA":
+    //         system.style.backgroundImage = "url('img/3.png')"
+    //         break
+    //     case "MASTERCARD":
+    //         system.style.backgroundImage = "url('img/4.png')"
+    //         break
+    // }
+    // document.querySelector(".card__number").textContent = format(form[2].value)
+    // document.querySelector(".card__expire").textContent = form[4].value
+    // document.querySelector(".card__name").textContent = form[3].value.toUpperCase()
+})
+
+document.querySelector(".input__bank").addEventListener("input", e => {
     switch (form[0].value.toUpperCase()) {
         case "ТИНЬКОФФ":
             card.style.backgroundImage = "url('img/1.png')"
@@ -11,6 +41,10 @@ form.addEventListener("submit", e => {
             card.style.backgroundImage = "url('img/2.png')"
             break
     }
+    console.log('++')
+})
+
+document.querySelector(".input__paymentType").addEventListener("input", e => {
     switch (form[1].value.toUpperCase()) {
         case "VISA":
             system.style.backgroundImage = "url('img/3.png')"
@@ -19,18 +53,27 @@ form.addEventListener("submit", e => {
             system.style.backgroundImage = "url('img/4.png')"
             break
     }
-    document.querySelector(".card__number").textContent = format(form[2].value)
-    document.querySelector(".card__expire").textContent = form[4].value
-    document.querySelector(".card__name").textContent = form[3].value.toUpperCase()
+    console.log('++')
 })
 
-form.addEventListener("reset", e => {
-    card.style.backgroundImage = ""
-    system.style.backgroundImage = ""
-    document.querySelector(".card__number").textContent = ""
-    document.querySelector(".card__expire").textContent = ""
-    document.querySelector(".card__name").textContent = ""
-    addTable(form)
+document.querySelector(".input__num").addEventListener("input", e => {
+    document.querySelector(".card__number").textContent = e.target.value
+    format(document.querySelector(".card__number").textContent)
+    console.log('++')
+})
+
+document.querySelector(".input__num").addEventListener("change", e => {
+    document.querySelector(".card__number").textContent = format(document.querySelector(".card__number").textContent)
+})
+
+document.querySelector(".input__name").addEventListener("input", e => {
+    document.querySelector(".card__name").textContent = document.querySelector(".input__name").value.toUpperCase()
+    console.log('++')
+})
+
+document.querySelector(".input__expire").addEventListener("input", e => {
+    document.querySelector(".card__expire").textContent = document.querySelector(".input__expire").value
+        console.log('++')
 })
 
 function format(s) {
