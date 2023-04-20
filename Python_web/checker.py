dcda0cc411fb67db0cdcbcf5c8ef8563171c0158
Rolan Zaripov <buldozerr21@gmail.com>
2023-04-20 13:17:03 +0300

    Revert "python"
    This reverts commit 95daed976a4b0851c4d3e3278166aeaad98a2a9c.

--- Python_web/checker.py
+++
@@ -1,7 +0,0 @@
-from flask import session
-def check_logged_in(func):
-    def wrapper():
-        if 'logged_in' in session:
-            return func()
-        return 'you are not logged in'
-    return wrapper()
